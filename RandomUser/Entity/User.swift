e8f90b12ea6a5c6c777e16b2b737e3018e116cb9
Andrianto Saputra Gunawan <andriantosg@gmail.com>
2020-12-01 13:45:47 +0700

    Add avatar, name, email and phone on user detail view

--- RandomUser/Entity/User.swift
+++ RandomUser/Entity/User.swift
@@ -8,39 +8,18 @@
 import Foundation
 
 struct User: Codable {
-    let gender: Gender
     let name: Name
-    let location: Location
     let email: String
     let phone: String
     let picture: Picture
 }
 
 extension User {
-    enum Gender: String, Codable {
-        case male
-        case female
-    }
-    
     struct Name: Codable {
         let title: String
         let first: String
         let last: String
     }
-    
-    struct Location: Codable {
-        let street: Street
-        let city: String
-        let state: String
-        let country: String
-        let postcode: Int
-        
-        struct Street: Codable {
-            let number: Int
-            let name: String
-        }
-    }
-    
     struct Picture: Codable {
         let large: URL
         let medium: URL
--- RandomUser/Modules/UserDetail/UserDetailView.swift
+++ RandomUser/Modules/UserDetail/UserDetailView.swift
@@ -6,19 +6,33 @@
 //
 
 import UIKit
+import Kingfisher
 
 class UserDetailView: UIViewController, ViewInterface {
     var presenter: UserDetailPresenterViewInterface?
-
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         presenter?.onViewDidLoad()
+    }
+    
+    private func setupView() {
+        title = "Random User Detail"
+        avatarImageView.layer.cornerRadius = 60.0
     }
 }
 
 extension UserDetailView: UserDetailViewPresenterInterface {
     func showData(user: User) {
-        // Show User detail here
-        title = user.name.first
+        avatarImageView.kf.setImage(with: user.picture.medium)
+        nameLabel.text = "\(user.name.title). \(user.name.first) \(user.name.last)"
+        emailLabel.text = user.email
+        phoneLabel.text = user.phone
     }
 }
